4587daab2960a939570c418471a32f8ed107f75b
Shiv Jain <70323345+shivjain123@users.noreply.github.com>
2021-09-12 11:25:22 +0530

    Update interpreting.py

--- interpreting.py
+++ interpreting.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly_express as px
+from matplotlib import pyplot as plt
 
 df = pd.read_csv('CSV Files/filtered.csv')
 
@@ -11,14 +12,46 @@ mass_list = df["Mass"].tolist()
 radius_list = df["Radius"].tolist()
 gravity_list = df["Gravity"].tolist()
 
-name_vs_mass = px.bar(x = name_list, y = mass_list)
-#name_vs_mass.show()
+name_vs_mass = plt.figure(figsize=(10, 5))
+name_vs_mass = plt.bar(name_list, mass_list)
+
+plt.xlabel("Planet Names")
+plt.ylabel("Planet Masses")
+plt.title("Name Vs Mass")
+plt.show()
+
+name_vs_distance = plt.figure(figsize=(10, 5))
+name_vs_distance = plt.bar(name_list, distance_list)
+
+plt.xlabel("Planet Names")
+plt.ylabel("Planet Distances")
+plt.title("Name Vs Distance")
+plt.show()
+
+name_vs_radius = plt.figure(figsize=(10, 5))
+name_vs_radius = plt.bar(name_list, radius_list)
+
+plt.xlabel("Planet Names")
+plt.ylabel("Planet Radiuses")
+plt.title("Name Vs Radius")
+plt.show()
+
+name_vs_gravity = plt.figure(figsize=(10, 5))
+name_vs_gravity = plt.bar(name_list, gravity_list)
+
+plt.xlabel("Planet Names")
+plt.ylabel("Planet Gravities")
+plt.title("Name Vs Gravity")
+plt.show()
+
+""" name_vs_mass = px.bar(x = name_list, y = mass_list)
+name_vs_mass.show()
 
 name_vs_distance = px.bar(x = name_list, y = distance_list)
-#name_vs_distance.show()
+name_vs_distance.show()
 
 name_vs_radius = px.bar(x = name_list, y = radius_list)
-#name_vs_radius.show()
+name_vs_radius.show()
 
 name_vs_gravity = px.bar(x=name_list, y=gravity_list)
-#name_vs_gravity.show()
+name_vs_gravity.show() """
